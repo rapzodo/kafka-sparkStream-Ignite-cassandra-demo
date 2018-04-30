ccd5d6b9b06ed2cbdfa3e561a70bab6bb804242e
danilo <rapzodo@gmail.com>
2018-04-30 15:04:30 +0200

    adding JsonConverter for events messages

---
+++ src/main/java/com/gridu/stopbot/converters/JsonConverter.java
@@ -0,0 +1,34 @@
+package com.gridu.stopbot.converters;
+
+import com.gridu.stopbot.model.Event;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class JsonConverter {
+    private static Logger LOG = LoggerFactory.getLogger(Event.class);
+
+    public static Event fromJson(String jsonString){
+        try {
+            jsonString = jsonString.replace("\"\"","\"");
+            jsonString = jsonString.replace("\\","/");
+            return new ObjectMapper().readValue(jsonString,Event.class);
+        } catch (IOException e) {
+            LOG.error("Error parsing Json > event :" + jsonString, e);
+        }
+        return null;
+    }
+
+    public static String toJson(Event event){
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(event);
+        } catch (IOException e) {
+            LOG.error("Error parsing Event > Json", e);
+        }
+        return null;
+    }
+}
